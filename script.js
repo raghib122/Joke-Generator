7463ec80609de62c27efa4c842ea5b63d5b62846
raghib122 <125207689+raghib122@users.noreply.github.com>
2023-06-12 21:46:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const button = document.querySelector(".container button");
+const buttonNext = document.getElementById("next");
+const buttonPrevious = document.getElementById("previous");
 const jokeDiv = document.querySelector(".container .joke p");
-document.addEventListener("DOMContentLoaded", getJock);
-button.addEventListener("click", getJock);
-async function getJock() {
-    const jokeData = await fetch("https://icanhazdadjoke.com/", {
-        headers: { Accept: "application/json" },
-    });
-    const jokeObj = await jokeData.json();
-    jokeDiv.innerHTML = jokeObj.joke;
-    console.log(jokeData);
+
+let jokeIndex = 0;
+let jokesData = [];
+
+document.addEventListener("DOMContentLoaded", getJokes);
+buttonNext.addEventListener("click", getNextJoke);
+buttonPrevious.addEventListener("click", getPreviousJoke);
+
+async function getJokes() {
+  const jokesDataResponse = await fetch("https://icanhazdadjoke.com/search", {
+    headers: { Accept: "application/json" },
+  });
+  const jokesDataObj = await jokesDataResponse.json();
+  jokesData = jokesDataObj.results;
+  displayJoke(jokeIndex);
+}
+
+function displayJoke(index) {
+  jokeDiv.innerHTML = jokesData[index].joke;
+}
+
+function getNextJoke() {
+  jokeIndex = (jokeIndex + 1) % jokesData.length;
+  displayJoke(jokeIndex);
+}
+
+function getPreviousJoke() {
+  jokeIndex = (jokeIndex - 1 + jokesData.length) % jokesData.length;
+  displayJoke(jokeIndex);
 }
